014a18033f0968ba38926fb6f75e84fca16b96b3
Joshua Guinness <joshua.guinness@gmail.com>
2020-04-06 22:12:17 -0400

    Updated stats model

--- Assets/Scripts/Models/Stats.cs
+++ Assets/Scripts/Models/Stats.cs
@@ -6,7 +6,8 @@ public enum CharacterType
 {
     PartyAnimal,
     GymRat,
-    Nerd
+    Bookworm,
+    CentroConnoisseur
 }
 
 public class Stats
@@ -17,6 +18,7 @@ public class Stats
     public int hunger;
     public int social;
     public int macCoins;
+    public int semesterCount;
 
     public Stats()
     {
@@ -26,5 +28,61 @@ public class Stats
         this.hunger = 50;
         this.social = 50;
         this.macCoins = 0;
+        this.semesterCount = 0;
+    }
+
+    public void setCharacterType(string type)
+    {
+        //this.characterType = CharacterType.
+    }
+
+    public void setHealth(int health)
+    {
+        this.health = health;
+    }
+
+    public void setGrade(int grade)
+    {
+        this.grade = grade;
+    }
+
+    public void setHunger(int hunger)
+    {
+        this.hunger = hunger;
+    }
+
+    public void setSocial(int social)
+    {
+        this.social = social;
+    }
+
+    public void getDisplayInfo()
+    {
+        // return
+    } 
+
+    public void subtractMacCoins(int amount)
+    {
+        this.macCoins -= amount;
+    }
+
+    public void addMacCoints(int amount)
+    {
+        this.macCoins += amount;
+    }
+
+    public int getMacCoins()
+    {
+        return this.macCoins;
+    }
+
+    public int getSemesterCount()
+    {
+        return this.semesterCount;
+    }
+    
+    public void incrementSemesterCount()
+    {
+        this.semesterCount += 1;
     }
 }
